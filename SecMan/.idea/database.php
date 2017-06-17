3c567d5b140eef1eb0d9b78510c24798f7ec4a88
nirvitlem <nvitlem@gmail.com>
2017-06-17 18:18:14 +0300

    1

---
+++ SecMan/.idea/database.php
@@ -0,0 +1,15 @@
+<?php
+$connect=mysqli_connect('localhost','root','','secmandb');
+
+if(mysqli_connect_errno($connect))
+{
+    echo 'Failed to connect';
+}
+
+?><?php
+/**
+ * Created by PhpStorm.
+ * User: NirV
+ * Date: 17/06/2017
+ * Time: 18:15
+ */
