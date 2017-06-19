99c45e5d92cc9e4668c519e9358c81d774f47485
nirvitlem <nvitlem@gmail.com>
2017-06-19 23:06:39 +0300

    19062917

--- SecMan/.idea/DBConnect.php
+++ SecMan/.idea/DBConnect.php
@@ -47,93 +47,36 @@ class DBConnect
 <html>
 <head>
     <style>
-label{display:inline-block;width:100px;margin-bottom:10px;}
+        label{display:inline-block;width:100px;margin-bottom:10px;}
     </style>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <div>
+    <?php  echo "<link rel='stylesheet' type='text/css' href='http://localhost/formcss.css' />"; ?>
 
+    <form action="index.php?t=<?php echo $tableName ?>" method="post">
+        <?php
 
+        $index=0;
+        foreach ($TableArrayKeyValues as $x=>$x_value)
+        {
+            if ($x!=$pKey)
+            {
+                echo "<input type=".$typeArray[$index]." name=".$x."/>";
+                echo "<label> ".$x_value." </label>";
+                echo "<br />";
+                $index++;
+            }
+        }
+
+        ?>
+        <input type="submit"   value ='<?php echo $buttonViewName; ?>' />
+
+    </form>
+    </div>
     <title><?php echo $titleHtmlViewName; ?></title>
 
-    <style type="text/css">
-    body {
-    font-size: 15px;
-            color: #343d44;
-            font-family: "segoe-ui", "open-sans", tahoma, arial;
-            padding: 0;
-            margin: 0;
-        }
-        table {
-    margin: auto;
-    font-family: "Lucida Sans Unicode", "Lucida Grande", "Segoe Ui";
-            font-size: 12px;
-        }
+     <?php  echo "<link rel='stylesheet' type='text/css' href='http://localhost/tablecss.css' />"; ?>
 
-        h1 {
-    margin: 25px auto 0;
-            text-align: center;
-            text-transform: uppercase;
-            font-size: 17px;
-        }
-
-        table td {
-    transition: all .5s;
-        }
-
-        /* Table */
-        .data-table {
-    border-collapse: collapse;
-            font-size: 14px;
-            min-width: 537px;
-        }
-
-        .data-table th,
-        .data-table td {
-    border: 1px solid #e1edff;
-            padding: 7px 17px;
-        }
-        .data-table caption {
-    margin: 7px;
-        }
-
-        /* Table Header */
-        .data-table thead th {
-    background-color: #508abb;
-            color: #FFFFFF;
-            border-color: #6ea1cc !important;
-            text-transform: uppercase;
-        }
-
-        /* Table Body */
-        .data-table tbody td {
-    color: #353535;
-}
-        .data-table tbody td:first-child,
-        .data-table tbody td:nth-child(4),
-        .data-table tbody td:last-child {
-    text-align: right;
-        }
-
-        .data-table tbody tr:nth-child(odd) td {
-    background-color: #f4fbff;
-        }
-        .data-table tbody tr:hover td {
-    background-color: #ffffa2;
-            border-color: #ffff0f;
-        }
-
-        /* Table Footer */
-        .data-table tfoot th {
-    background-color: #e5f5ff;
-            text-align: right;
-        }
-        .data-table tfoot th:first-child {
-    text-align: left;
-        }
-        .data-table tbody td:empty
-        {
-            background-color: #ffcccc;
-        }
-    </style>
 </head>
 <h1></h1>
 <h1></h1>
@@ -166,33 +109,12 @@ label{display:inline-block;width:100px;margin-bottom:10px;}
 <tfoot>
 </tfoot>
 </table>
-</head>
 <body>
 <h1></h1>
 <h1></h1>
-<div style="position: absolute; left: 200px; top: 50px;">
 
-<form action="index.php?t=<?php echo $tableName ?>" method="post">
-        <?php
 
-        $index=0;
-        foreach ($TableArrayKeyValues as $x=>$x_value)
-        {
-            if ($x!=$pKey)
-            {
-                echo "<input type=".$typeArray[$index]." name=".$x."/>";
-                echo "<label> ".$x_value." </label>";
-                echo "<br />";
-                $index++;
-            }
-        }
 
-        ?>
-        <input type="submit"   value ='<?php echo $buttonViewName; ?>' />
-
-</form>
-
-</div>
 
 </body>
 </html>
